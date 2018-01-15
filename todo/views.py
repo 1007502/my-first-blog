84a5257bc5c08e36a3c53178bed08d6e18517194
1007502 <ivo-geenen@hotmail.com>
2018-01-15 18:04:20 +0100

    My Django Girls app, first commit2

--- todo/views.py
+++ todo/views.py
@@ -220,57 +220,7 @@ def startpage(request):
     return render(request, 'todo/startpage.html')
 
 def login(request):
-        notes = Note.objects.filter(user=request.user)
-        tasks = Task.objects.filter(user=request.user)
-        food_balance =  Expense.objects.filter(catogory__iexact='food').aggregate(Sum('amount'))['amount__sum']
-        social_balance =  Expense.objects.filter(catogory__iexact='social life').aggregate(Sum('amount'))['amount__sum']
-        transport_balance =  Expense.objects.filter(catogory__iexact='transportation').aggregate(Sum('amount'))['amount__sum']
-        household_balance =  Expense.objects.filter(catogory__iexact='household').aggregate(Sum('amount'))['amount__sum']
-        culture_balance =  Expense.objects.filter(catogory__iexact='culture').aggregate(Sum('amount'))['amount__sum']
-        other_balance =  Expense.objects.filter(catogory__iexact='other').aggregate(Sum('amount'))['amount__sum']
-        income_total = Income.objects.aggregate(Sum('amount'))['amount__sum']
-        expense_total = Expense.objects.aggregate(Sum('amount'))['amount__sum']
-        if expense_total is None:
-            expense_total = 0.00
-        if income_total is None:
-            income_total = 0.00
-        incomes = Income.objects.filter(user=request.user).order_by('-id')
-        last_incomes = incomes[:3]
-        expenses = Expense.objects.filter(user=request.user).order_by('-id')
-        last_expenses = expenses[:3]
-        if not expenses and not incomes:
-            balance_total = 0
-        elif not incomes:
-            balance_total = - expense_total
-        elif not expenses:
-            balance_total = income_total
-        else:
-            balance_total = income_total -  expense_total
-        if balance_total < 0:
-            negative = True
-            balance_total = abs(balance_total)
-        else:
-            negative = False
-
-        context = {
-        'expenses': expenses,
-        'incomes' : incomes,
-        'balance_total': balance_total,
-        'negative' : negative,
-        'last_incomes' : last_incomes,
-        'last_expenses' : last_expenses,
-        'food_balance' : food_balance,
-        'social_balance' : social_balance,
-        'transport_balance' : transport_balance,
-        'household_balance' : household_balance,
-        'culture_balance' : culture_balance,
-        'other_balance' : other_balance,
-        'income_total' : income_total,
-        'expense_total' : expense_total,
-        'tasks' : tasks,
-        'notes' : notes,
-        }
-        return render(request, 'todo/index.html', context)
+    return render(request, 'todo/index.html')
 
 def logout(request):
     logout(request)
@@ -285,10 +235,13 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request)
-            return render(request, 'todo/index.html')
+            return render(request, 'todo/startpage.html')
     else:
         form = UserCreationForm()
     return render(request, 'todo/signup.html', {'form': form})
+
+def video(request):
+    return render(request, 'todo/video.html')
 
 def about(request):
     if not request.user.is_authenticated:
